9c1603b930f310bb1e38201b9be799d3a2f92e4b
Sarwesh Karande <45988700+saturn279@users.noreply.github.com>
2022-03-17 17:29:25 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+mkdir ~/dnfuzz
+cd ~/dnfuzz && { curl -O https://raw.githubusercontent.com/saturn279/dnfuzz/main/dnfuzz.sh ; cd -; }
+echo -n "alias dnfuzz='bash ~/dnfuzz/dnfuzz.sh'" >> .bashrc
+echo -n "alias dnfuzz='bash ~/dnfuzz/dnfuzz.sh'" >> .zshrc
+bash -c "alias dnfuzz='bash ~/dnfuzz/dnfuzz.sh'"
+zsh -c "alias dnfuzz='bash ~/dnfuzz/dnfuzz.sh'"
+
