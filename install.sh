1873624ddf68bb58c0680cfecd5757696a919917
Sarwesh Karande <45988700+saturn279@users.noreply.github.com>
2022-03-21 21:06:32 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir ~/dnfuzz
 cd ~/dnfuzz && { curl -O https://raw.githubusercontent.com/saturn279/dnfuzz/main/dnfuzz.sh ; cd -; }
-echo -n "alias dnfuzz='bash ~/dnfuzz/dnfuzz.sh'" >> ~/.bashrc
-echo -n "alias dnfuzz='bash ~/dnfuzz/dnfuzz.sh'" >> ~/.zshrc
+echo "alias dnfuzz='bash ~/dnfuzz/dnfuzz.sh'" >> ~/.bashrc
+echo "alias dnfuzz='bash ~/dnfuzz/dnfuzz.sh'" >> ~/.zshrc
 printf "\nRestart your terminal to finish installation.\n"
 
