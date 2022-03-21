7479f9b5150e4b1f2be211d0e32c3ab49c4ea352
Sarwesh Karande <45988700+saturn279@users.noreply.github.com>
2022-03-21 21:00:27 +0530

    Multithreading support, exponential performance improvements, redesign, better help section

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # dnfuzz
-Subdomain and hidden directory checking tool. 
+Subdomain and hidden directory checking tool with multithreading support.
 
 # Installation
 
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/saturn279/dnfuzz/main/install.sh)"
     
 # Description
-  **dnfuzz** is an command line script for recon which can loop through a list of subdomains while performing directory fuzzing.
+  **dnfuzz** is an command line script for recon which can search through a list of subdomains while performing directory fuzzing.
     
-    dnfuzz -d DOMAIN [OPTIONS]  
+    dnfuzz -d <domain> -subs <subdomain_list> -dir <directory_list> -P <threadcount> 
 # Options
     -h                                   Print this help text and exit
     -d                                   Domain name [example.com] 
     -subs                                Text file with list of subdomains [subslist.txt]
     -dir                                 Text file with list of directories [dirslist.txt]
-    
+    -P                                   Number of threads for xargs
--- dnfuzz.sh
+++ dnfuzz.sh
@@ -1,73 +1,73 @@
 #!/bin/bash
 function help() {
-        echo "+++++++++++++++++++++++++"
-        echo "Subdomain Enum & Fuzzing"
-        echo "+++++++++++++++++++++++++"
-        echo "-d                :       Domain as target"
-        echo "-subs             :       Subdomain wordlist"
-        echo "-dir              :       Dir wordlist"
+    echo "---------Subdomain and hidden directory checking tool---------"
+    echo "Usage: dnfuzz -d <domain> -subs <subdomain_list> -dir <directory_list> -P <threadcount>"
+    echo "    -h                           Print this help text and exit"
+    echo "    -d                           Domain name [example.com] "
+    echo "    -subs                        Text file with list of subdomains [subslist.txt]"
+    echo "    -dir                         Text file with list of directories [dirslist.txt]"
+    echo "    -P                           Number of threads for xargs"
+    echo "Example: dnfuzz -d github.com -subs subslist.txt -dir dirslist.txt -P 500"
 }
 function tools() {
-        type -P host &>/dev/null
-        if [ ! $? -eq 0 ]; then
-                echo "Error: Please install host command and run again."
-                exit 127
-        fi
-        type -P curl &>/dev/null
-        if [ ! $? -eq 0 ]; then
-                echo "Error: Please install cURL command and run again."
-                exit 127
-        fi
+    type -P host &>/dev/null
+    if [ ! $? -eq 0 ]; then
+        echo "Error: Please install host command and run again."
+        exit 127
+    fi
+    type -P curl &>/dev/null
+    if [ ! $? -eq 0 ]; then
+        echo "Error: Please install cURL command and run again."
+        exit 127
+    fi
+
 }
 function main() {
-        while read -r subs; do
-                if host $subs.$target &>/dev/null; then
-                        while read -r dir; do
-                                code=$(curl -ks "https://$subs.$target/$dir" -w "%{http_code}" -o /dev/null)
-                                if [ "$code" -eq "200" ]; then
-                                        echo "https://$subs.$target/$dir  : 200 "
-                                fi
-                        done <$dir_list
-                fi
-        done <$subdomain_list
+
+    xargs -a $subdomain_list -I@ -P$threads sh -c "host \"@\".$target" | grep -wv 'not found' | awk '{print $1}' | sort -u | xargs -I@ -P$threads sh -c "xargs -a $dir_list -I^  -P10 sh -c 'echo \"curl -ks https://\"@\"/\"^\" -w \"%{http_code}\" -o /dev/null\";'" | xargs -I@ -P500 sh -c 'codeh=$(@);if [ "$codeh" -eq "200" ]; then echo "@";  fi' | awk '{print $3}'
+
 }
 function arg() {
-        while :; do
-                case $1 in
-                '-d')
-                        target=$2
-                        shift
-                        ;;
-                '-subs')
-                        subdomain_list=$2
-                        shift
-                        ;;
-                '-dir')
-                        dir_list=$2
-                        shift
-                        ;;
-                '-h')
-                        help
-                        exit 0
-                        ;;
-                "")
-                        shift
-                        break
-                        ;;
-                *)
-                        echo "Error: Unknown Option: $1"
-                        echo "Usage: bash script -h"
-                        exit 1
-                        ;;
-                esac
-                shift
-        done
+    while :; do
+        case $1 in
+        '-d')
+            target=$2
+            shift
+            ;;
+        '-subs')
+            subdomain_list=$2
+            shift
+            ;;
+        '-dir')
+            dir_list=$2
+            shift
+            ;;
+        '-P')
+            threads=$2
+            shift
+            ;;
+        '-h')
+            help
+            exit 0
+            ;;
+        "")
+            shift
+            break
+            ;;
+        *)
+            echo "Error: Unknown Option: $1"
+            echo "Usage: bash script -h"
+            exit 1
+            ;;
+        esac
+        shift
+    done
 }
 arg $@
 tools
 if [ ! -z "$target" ]; then
-        main
+    main
 else
-        echo "[!]Error: please provide domain, or look for help -h"
-        exit 1
+    echo "[!]Error: please provide domain, subslist, dirslist, threadcount, or look for help -h"
+    exit 1
 fi
